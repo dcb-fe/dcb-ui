aa54f1f55a8a95a1f9b714f7c22d8cf3eb3232ff
方剑成 <fjc0kb@gmail.com>
2021-06-18 16:34:36 +0800

    up

---
+++ notify-dingtalk.config.js
@@ -0,0 +1,7 @@
+module.exports = require('notify-dingtalk').defineConfig({
+  accessToken:
+    'a9303b222fa1be77e06b1023449773e970911e4c30890a750225e0408adef61a',
+  secret: 'SEC9ddb2b08ce79b6c2429532e477aa82e5b0cc0a8baf7ae9da8f93385b52e551b0',
+  title: '发布通知',
+  content: '测试',
+});
