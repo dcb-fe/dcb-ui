0a6802811683540b58b7bb2d576c5fd438fdb0b1
方剑成 <fjc0kb@gmail.com>
2021-06-18 16:36:56 +0800

    up

--- notify-dingtalk.config.js
+++ notify-dingtalk.config.js
@@ -1,4 +1,6 @@
-module.exports = require('notify-dingtalk').defineConfig({
+const { defineConfig } = require('notify-dingtalk');
+
+module.exports = defineConfig({
   accessToken:
     'a9303b222fa1be77e06b1023449773e970911e4c30890a750225e0408adef61a',
   secret: 'SEC9ddb2b08ce79b6c2429532e477aa82e5b0cc0a8baf7ae9da8f93385b52e551b0',
