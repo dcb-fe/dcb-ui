5a884a4a007291fd1cee56bbc460273ae236540d
方剑成 <fjc0kb@gmail.com>
2021-06-21 17:07:27 +0800

    up

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: '多彩宝移动端 UI 组件库',
+  description: '多彩宝移动端 UI 组件库',
+  port: 9988,
+  themeConfig: {
+    nav: [
+      { text: '使用指南', link: '/guide.html' },
+      { text: '组件', link: '/components/' },
+      { text: '更新日志', link: '/changelog.html' },
+    ],
+    sidebar: {},
+    lastUpdated: '上次更新时间',
+  },
+};
