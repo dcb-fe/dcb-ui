cc362c3a5fde6ec3b2d25eb7da0cf826d26a59bc
方剑成 <fjc0kb@gmail.com>
2021-06-22 16:37:15 +0800

    up

--- haoma-compile.config.js
+++ haoma-compile.config.js
@@ -1,12 +1,63 @@
 const { basename, extname } = require('path');
+const { defineBabelPlugin, getCompileConfig } = require('haoma');
 
-module.exports = require('haoma').getCompileConfig([
+const removeVueDocHelpersPlugin = defineBabelPlugin(t => ({
+  visitor: {
+    CallExpression: {
+      exit(path) {
+        if (
+          t.isIdentifier(path.node.callee) &&
+          path.node.callee.name === 'defineComponent' &&
+          path.node.arguments.length &&
+          t.isObjectExpression(path.node.arguments[0])
+        ) {
+          const postRuns = [];
+          path.node.arguments[0].properties.forEach((prop, index) => {
+            if (t.isObjectProperty(prop) && t.isIdentifier(prop.key)) {
+              // 移除 slots, emits
+              if (['slots', 'emits'].includes(prop.key.name)) {
+                postRuns.unshift(() => {
+                  path.node.arguments[0].properties.splice(index, 1);
+                });
+              }
+              // 移除 props 下的 enum, desc
+              else if (
+                prop.key.name === 'props' &&
+                t.isObjectExpression(prop.value)
+              ) {
+                prop.value.properties.forEach(p => {
+                  if (t.isObjectProperty(p) && t.isObjectExpression(p.value)) {
+                    p.value.properties.forEach((col, colIndex) => {
+                      if (
+                        t.isObjectProperty(col) &&
+                        t.isIdentifier(col.key) &&
+                        ['enum', 'desc'].includes(col.key.name)
+                      ) {
+                        postRuns.unshift(() => {
+                          p.value.properties.splice(colIndex, 1);
+                        });
+                      }
+                    });
+                  }
+                });
+              }
+            }
+          });
+          postRuns.forEach(run => run());
+        }
+      },
+    },
+  },
+}));
+
+module.exports = getCompileConfig([
   {
     name: 'esm',
     inputFiles: ['./src/**/*.{js,vue}', '!**/__*/**', '!**/*.test.*'],
     outDir: 'lib',
     module: 'esm',
     target: 'browser',
+    plugins: [removeVueDocHelpersPlugin],
     getCssModulesScopedName: ({ className, fileName }) => {
       const componentName = basename(fileName, extname(fileName));
       const prefix = className === componentName ? '' : `${componentName}-`;
@@ -19,6 +70,7 @@ module.exports = require('haoma').getCompileConfig([
     outDir: 'lib/_cjs',
     module: 'cjs',
     target: 'browser',
+    plugins: [removeVueDocHelpersPlugin],
     getCssModulesScopedName: ({ className, fileName }) => {
       const componentName = basename(fileName, extname(fileName));
       const prefix = className === componentName ? '' : `${componentName}-`;
