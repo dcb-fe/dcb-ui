50484d52b2d86aa38ce02fb396d389671d88f7b1
方剑成 <fjc0kb@gmail.com>
2021-06-24 13:57:06 +0800

    up

--- jest.config.js
+++ jest.config.js
@@ -7,28 +7,40 @@ module.exports = require('haoma').getJestConfig({
     'vue-jest': {
       transform: {
         // https://github.com/vuejs/vue-jest/blob/v4.0.1/lib/process-style.js#L83
-        '^scss$': {
+        '^(scss|less|css)$': {
           process(content) {
             return content;
           },
-          postprocess(content, path) {
-            const clsJson = makeSynchronous(async (content, path) => {
-              const { renderStyle } = require('haoma');
-              const { basename, extname } = require('path');
-              const { cls } = await renderStyle({
-                lang: 'scss',
-                filePath: path,
-                fileContent: content,
-                cssModules: true,
-                cssModulesScopedName: ({ className, filePath }) => {
-                  const componentName = basename(filePath, extname(filePath));
-                  const prefix =
-                    className === componentName ? '' : `${componentName}-`;
-                  return `d-${prefix}${className}`;
-                },
-              });
-              return JSON.stringify(cls);
-            })(content, path);
+          postprocess(content, path, _, { lang, module }) {
+            const clsJson = makeSynchronous(
+              async (content, path, lang, module) => {
+                const { renderStyle } = require('haoma');
+                const { basename, extname } = require('path');
+                const { cls } = await renderStyle({
+                  lang:
+                    lang === 'scss' ? 'scss' : lang === 'less' ? 'less' : 'css',
+                  filePath: path,
+                  fileContent: content,
+                  ...(!module
+                    ? {}
+                    : {
+                        cssModules: true,
+                        cssModulesScopedName: ({ className, filePath }) => {
+                          const componentName = basename(
+                            filePath,
+                            extname(filePath),
+                          );
+                          const prefix =
+                            className === componentName
+                              ? ''
+                              : `${componentName}-`;
+                          return `d-${prefix}${className}`;
+                        },
+                      }),
+                });
+                return JSON.stringify(cls);
+              },
+            )(content, path, lang, module);
             return clsJson;
           },
         },
