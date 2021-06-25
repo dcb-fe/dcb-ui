b9a2f1fa1f9cf9eb12bd5f481d254a56f3d844cb
方剑成 <fjc0kb@gmail.com>
2021-06-25 11:57:55 +0800

    fix: 同步组件文档

--- scripts/sync_docs.js
+++ scripts/sync_docs.js
@@ -2,11 +2,13 @@ import chokidar from 'chokidar';
 import exec from 'execa';
 import fs from 'fs-extra';
 import path from 'path';
-import { debounce } from 'vtils';
+import { debounce, wait } from 'vtils';
 
 async function main() {
+  const isOnce = process.argv.includes('--once');
+
   // 复制组件文档
-  chokidar
+  const watch1 = chokidar
     .watch(path.join(__dirname, '../src/components/*/*.md'))
     .on('all', async (type, filePath) => {
       const [, $1, $2] = filePath.match(/^.+\/(.+?)\/(.+?)\.md$/);
@@ -19,12 +21,19 @@ async function main() {
     });
 
   // 更新组件演示
-  chokidar.watch(path.join(__dirname, '../src/components/*/__demo__/*.vue')).on(
-    'all',
-    debounce(async () => {
-      await exec('npm', ['run', 'gen-index']);
-    }, 60),
-  );
+  const watch2 = chokidar
+    .watch(path.join(__dirname, '../src/components/*/__demo__/*.vue'))
+    .on(
+      'all',
+      debounce(async () => {
+        await exec('npm', ['run', 'gen-index']);
+      }, 60),
+    );
+
+  if (isOnce) {
+    await wait(100);
+    await Promise.all([watch1.close(), watch2.close()]);
+  }
 }
 
 main();
