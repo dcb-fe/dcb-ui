3925ce8d6cc9d7f2a39cab4010cc5735a6ab6412
方剑成 <fjc0kb@gmail.com>
2021-06-28 17:26:51 +0800

    chore: test

--- src/components/form/form.test.js
+++ src/components/form/form.test.js
@@ -5,10 +5,15 @@ console.log(Form, shallowMount);
 
 describe('Form', () => {
   describe('视图', () => {
-    // ...
+    test('xx', () => {
+      expect(1 + 1).toBe(2);
+    });
   });
 
   describe('逻辑', () => {
     // ...
+    test('xx', () => {
+      expect(1 + 1).toBe(2);
+    });
   });
 });
