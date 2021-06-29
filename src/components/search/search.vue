fb50fb7e73c62d44ee7a213901abd9b1711a4eb8
xuzhenwu <xuzhenwu@gogpay.cn>
2021-06-29 18:12:18 +0800

    feat: 新增 Search 组件

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,16 +1,17 @@
 <template>
-  <div :class="_.search">...</div>
+  <div :class="_.search">123</div>
 </template>
 
 <script>
   import { defineComponent } from '@/utils';
 
   export default defineComponent({
-    name: 'Search',
+    name: 'Search'
   });
 </script>
 
 <style lang="scss" module>
   .search {
+    padding: 16px;
   }
 </style>
