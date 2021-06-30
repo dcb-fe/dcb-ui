8d62e553cd65bb2e2bac36c228b4e322d9387212
方剑成 <fjc0kb@gmail.com>
2021-06-30 18:23:31 +0800

    feat: 新增 Popup 弹出层组件

--- docs/.vuepress/demo.js
+++ docs/.vuepress/demo.js
@@ -4,6 +4,10 @@ export { default as DemoButtonDisabled } from '../../src/components/button/__dem
 export { default as DemoButtonPrimary } from '../../src/components/button/__demo__/primary';
 export { default as DemoButtonSecondary } from '../../src/components/button/__demo__/secondary';
 export { default as DemoButtonSize } from '../../src/components/button/__demo__/size';
+export { default as DemoPopupBasic } from '../../src/components/popup/__demo__/basic';
+export { default as DemoPopupCustomTransition } from '../../src/components/popup/__demo__/custom_transition';
+export { default as DemoPopupDrawer } from '../../src/components/popup/__demo__/drawer';
+export { default as DemoPopupTransparentMask } from '../../src/components/popup/__demo__/transparent_mask';
 // @endindex
 
 // @index(['../**/*.vue', '!**/.*/**'], (f, _) => `export { default as ${_.pascalCase('demo_docs_'+f.path)} } from '${f.path}';`)
