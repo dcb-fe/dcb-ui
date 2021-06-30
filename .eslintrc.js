f23bc75d4c4cf272d3e1a74003820fb3630cb4a9
Jay Fong <fjc0kb@gmail.com>
2021-06-30 12:53:57 +0800

    docs: 为组件文档添加贡献者展示 (#4)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,6 @@
 /** @type import('haoma').ESLintConfig */
-module.exports = require('haoma').getESLintConfig();
+module.exports = require('haoma').getESLintConfig({
+  rules: {
+    'vue/html-self-closing': 'off',
+  },
+});
