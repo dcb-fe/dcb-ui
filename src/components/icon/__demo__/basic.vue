f5f368eed20a04dfe47d30ace61b98e5af5f70f7
code-snippet <562304744@qq.com>
2021-07-12 13:03:13 +0800

    feat: 新增 Icon 图标组件 (#10)
    * feat: 新增icon组件
    
    * feat: 新增icon
    
    * feat(Icon): 完善属性、文档
    
    * chore: tweaks
    
    * fix(Icon): 样式调整
    
    Co-authored-by: duanshuwen <duanshuwen@gogpay.cn>
    Co-authored-by: 方剑成 <fjc0kb@gmail.com>

---
+++ src/components/icon/__demo__/basic.vue
@@ -0,0 +1,70 @@
+<template>
+  <ul class="list">
+    <li
+      v-for="item in icons"
+      :key="item"
+      class="item"
+      @click="handleClick(item)"
+    >
+      <d-icon :name="item" class="icon" />
+      <div class="name">{{ item }}</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+  import { copyTextToClipboard } from 'vtils';
+  import { Icon } from '@dcbfe/ui';
+
+  export default {
+    data() {
+      return {
+        icons: Icon.props.name.enum,
+      };
+    },
+    methods: {
+      handleClick(name) {
+        copyTextToClipboard(`<d-icon name="${name}" />`);
+        alert('已复制');
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    .item {
+      border: 1px solid #e8e8e8;
+      height: 100px;
+      flex: 0 0 100px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin: 5px 0;
+      cursor: pointer;
+      background-color: white;
+      border-radius: 4px;
+
+      .icon {
+        font-size: 24px;
+      }
+
+      .name {
+        font-size: 12px;
+        white-space: nowrap;
+      }
+
+      &:active {
+        background-color: mix(black, white, 10%);
+      }
+    }
+  }
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 // @index(['./**/*.vue', '!**/__*/**'], (f, _) => `export { default as ${_.pascalCase(f.name)} } from '${f.path}';`)
 export { default as Button } from './button/button';
+export { default as Icon } from './icon/icon';
 export { default as Popup } from './popup/popup';
 // @endindex
