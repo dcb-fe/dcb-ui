526eda7509b0f3cc0cd8e4a7777bd7be4a1badd0
gaorongjun@gogpay.cn <gaorongjun@gogpay.cn>
2021-07-13 11:22:04 +0800

    fix: 演示demo 代码格式优化

--- src/components/number_keyboard/__demo__/def.vue
+++ src/components/number_keyboard/__demo__/def.vue
@@ -1,36 +1,72 @@
 <template>
   <div class="test">
     <ul class="card">
-      <li @click.stop.prevent="showFn(1)"><span>弹出默认键盘</span>
+      <li @click.stop.prevent="showFn(1)">
+        <span>弹出默认键盘</span>
         <d-icon name="return"/>
       </li>
-      <li @click.stop.prevent="showFn(2)"><span>弹出带右侧栏键盘</span>
+      <li @click.stop.prevent="showFn(2)">
+        <span>弹出带右侧栏键盘</span>
         <d-icon name="return"/>
       </li>
-      <li @click.stop.prevent="showFn(3)"><span>弹出身份证号键盘</span>
+      <li @click.stop.prevent="showFn(3)">
+        <span>弹出身份证号键盘</span>
         <d-icon name="return"/>
       </li>
-      <li @click.stop.prevent="showFn(4)"><span>弹出带标题键盘</span>
+      <li @click.stop.prevent="showFn(4)">
+        <span>弹出带标题键盘</span>
         <d-icon name="return"/>
       </li>
-      <li @click.stop.prevent="showFn(5)"><span>弹出随机键盘</span>
+      <li @click.stop.prevent="showFn(5)">
+        <span>弹出随机键盘</span>
         <d-icon name="return"/>
       </li>
-      <li @click.stop.prevent="showFn(6)"><span>弹出配置多个按键的键盘</span>
+      <li @click.stop.prevent="showFn(6)">
+        <span>弹出配置多个按键的键盘</span>
         <d-icon name="return"/>
       </li>
-      <li @click.stop.prevent="showFn(7)"><span>双向绑定<input placeholder="点此输入" readonly v-model="text"/></span></li>
+      <li @click.stop.prevent="showFn(7)">
+        <span>双向绑定<input placeholder="点此输入" readonly v-model="text"/></span>
+      </li>
     </ul>
-    <d-number-keyboard @blur="show1 = false" @close="show1 = false" :show="show1"></d-number-keyboard>
-    <d-number-keyboard extraKey="." @close="show2 = false" :show="show2" theme="custom"></d-number-keyboard>
-    <d-number-keyboard extraKey="X" closeButtonText="完成" @close="show3 = false" :show="show3"></d-number-keyboard>
-    <d-number-keyboard title="标题" closeButtonText="完成" @close="show4 = false" extraKey="."
-                       :show="show4"></d-number-keyboard>
-    <d-number-keyboard :show="show5" @close="show5 = false" :randomKey="true"></d-number-keyboard>
-    <d-number-keyboard :extraKey="['0','0']" closeButtonText="完成" @close="show6 = false" :show="show6"
-                       :randomKey="false"></d-number-keyboard>
-    <d-number-keyboard v-model="text" @close="show7 = false" closeButtonText="完成" @select="select" :extraKey="['0','0']"
-                       :show="show7" :randomKey="true"></d-number-keyboard>
+    <d-number-keyboard @blur="show1 = false"
+                       @close="show1 = false"
+                       :show="show1">
+    </d-number-keyboard>
+    <d-number-keyboard theme="custom"
+                       extraKey="."
+                       @close="show2 = false"
+                       :show="show2">
+    </d-number-keyboard>
+    <d-number-keyboard extraKey="X"
+                       closeButtonText="完成"
+                       @close="show3 = false"
+                       :show="show3">
+    </d-number-keyboard>
+    <d-number-keyboard title="标题"
+                       closeButtonText="完成"
+                       extraKey="."
+                       @close="show4 = false"
+                       :show="show4">
+    </d-number-keyboard>
+    <d-number-keyboard @close="show5 = false"
+                       :show="show5"
+                       :randomKey="true">
+    </d-number-keyboard>
+    <d-number-keyboard closeButtonText="完成"
+                       @close="show6 = false"
+                       :extraKey="['0','0']"
+                       :show="show6"
+                       :randomKey="false">
+    </d-number-keyboard>
+    <d-number-keyboard v-model="text"
+                       closeButtonText="完成"
+                       @close="show7 = false"
+                       @select="select"
+                       :extraKey="['0','0']"
+                       :show="show7"
+                       :randomKey="true">
+    </d-number-keyboard>
   </div>
 </template>
 <!--test-->
