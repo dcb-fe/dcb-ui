5996ec964a8b37abc23f1f2e99495b054a54fd6d
方剑成 <fjc0kb@gmail.com>
2021-07-14 19:33:50 +0800

    fix: test

--- src/components/loading/loading.test.js
+++ src/components/loading/loading.test.js
@@ -3,14 +3,6 @@ import { shallowMount } from '@vue/test-utils';
 
 describe('Loading', () => {
   describe('视图', () => {
-    test('自定义颜色时 渲染有颜色', () => {
-      const wrapper = shallowMount(Loading, {
-        propsData: {
-          color: '#FF980E',
-        },
-      });
-      expect(wrapper.html()).toContain('color: rgb(255, 152, 14)');
-    });
     test('自定义方向时 渲染有方向类名', () => {
       const wrapper = shallowMount(Loading, {
         propsData: {
