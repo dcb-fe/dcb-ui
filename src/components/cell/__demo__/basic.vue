b147cb0890337be50330372bea1e61652d990b49
code-snippet <562304744@qq.com>
2021-07-27 14:54:43 +0800

    feat: 新增 AddressEdit 地址编辑 组件 (#28)
    Co-authored-by: duanshuwen <duanshuwen@gogpay.cn>

--- src/components/cell/__demo__/basic.vue
+++ src/components/cell/__demo__/basic.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <d-cell title="我是标题" desc="描述文字" left-icon="clock" right-icon="setting" @click="handleClick"/>
-    <d-cell title="我是标题" desc="描述文字" right-icon="setting" @click="handleClick">
+    <d-cell label="我是标题" desc="描述文字" left-icon="clock" right-icon="arrow_right" @click="handleClick"/>
+    <d-cell label="我是标题" desc="描述文字" right-icon="arrow_right" @click="handleClick">
       <input slot="desc" type="text" placeholder="请输入名字">
     </d-cell>
-    <d-cell title="是否设置为默认地址" desc="描述文字" @click="handleClick">
+    <d-cell label="是否设置为默认地址" desc="描述文字" @click="handleClick">
       <d-switch slot="desc" />
     </d-cell>
   </div>
