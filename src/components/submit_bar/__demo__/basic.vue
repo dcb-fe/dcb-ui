b42ea6fed84ba9090713068caaace57bdd82e924
刘杲 <liugao@gogpay.cn>
2021-09-01 16:38:42 +0800

    feat: 新增 submit_bar 组件

---
+++ src/components/submit_bar/__demo__/basic.vue
@@ -0,0 +1,21 @@
+<template>
+  <d-submit-bar price="30.5" button-text="提交订单" @submit="handleClick">
+  </d-submit-bar>
+</template>
+
+<script>
+  export default {
+    methods: {
+      handleClick() {
+        alert('hello');
+      },
+    },
+  };
+</script>
+
+<style lang="scss">
+  .d-submit_bar {
+    position: relative;
+    padding-bottom: 0;
+  }
+</style>
