fc7fa9c2b9ab10e9e75f4c1f6f4d68a92fd650a4
刘杲 <38686771+liugaol@users.noreply.github.com>
2021-09-08 16:16:10 +0800

    fix(GoodsAction): 添加目录 (#47)
    * feat: 新增 GoodsAction 组件
    
    * fix(GoodsAction): 添加目录
    
    Co-authored-by: 刘杲 <liugao@gogpay.cn>

---
+++ src/components/goods_action_button/__demo__/basic.vue
@@ -0,0 +1,13 @@
+<template>
+  <d-goods-action-button text="加入购物车" @click="handleClick" />
+</template>
+
+<script>
+  export default {
+    methods: {
+      handleClick() {
+        alert('hello');
+      },
+    },
+  };
+</script>
---
+++ src/components/goods_action_button/goods_action_button.vue
@@ -0,0 +1,45 @@
+<template>
+  <d-button
+    :class="_.goods_action_button"
+    :style="{ background: color }"
+    @click="$emit('click')"
+    >{{ text }}</d-button
+  >
+</template>
+
+<script>
+  import DButton from '@/components/button/button';
+  import { defineComponent } from '@/utils';
+
+  export default defineComponent({
+    name: 'GoodsActionButton',
+    components: {
+      DButton,
+    },
+    props: {
+      text: {
+        type: String,
+        default: '-',
+        desc: '按钮文字',
+      },
+      color: {
+        type: String,
+        default: 'linear-gradient(90deg, #FECE20 0%, #FF8A17 100%)',
+        desc: '按钮背景颜色',
+      },
+    },
+  });
+</script>
+
+<style lang="scss" module>
+  .goods_action_button {
+    flex: 1;
+    height: 38px;
+    font-size: 14px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: #ffffff;
+    line-height: 20px;
+    margin-right: 5px;
+  }
+</style>
