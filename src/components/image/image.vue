fcf865d0002bd77f9a133e575fdeef81fb057e73
username <75294703+WangYanzhi0525@users.noreply.github.com>
2021-09-14 14:50:04 +0800

    feat: 新增 steps 组件

---
+++ src/components/image/image.vue
@@ -0,0 +1,66 @@
+<template>
+  <div :class="_.image">...</div>
+</template>
+
+<script>
+  import { defineComponent } from '@/utils';
+  export default defineComponent({
+    name: 'Image',
+    props: {
+      src: {
+        type: String,
+        default: '',
+        desc: '图片链接',
+      },
+      fit: {
+        type: String,
+        default: 'fill',
+        desc: '图片填充模式',
+      },
+      alt: {
+        type: String,
+        default: '',
+        desc: '替代文本',
+      },
+      width: {
+        type: String,
+        enum: ['number', 'string'],
+        default: '',
+        desc: '宽度，默认单位为px',
+      },
+      height: {
+        type: String,
+        enum: ['number', 'string'],
+        default: '',
+        desc: '高度，默认单位为px',
+      },
+      radius: {
+        type: String,
+        enum: ['number', 'string'],
+        default: '',
+        desc: '圆角大小，默认单位为px',
+      },
+      round: {
+        type: Boolean,
+        default: true,
+        desc: '是否显示指示器',
+      },
+      vertical: {
+        type: Boolean,
+        default: false,
+        desc: '是否为纵向滚动',
+      },
+      indicatorPosition: {
+        type: String,
+        enum: ['left', 'right', 'top', 'bottom'],
+        default: 'bottom',
+        desc: '指示器位置',
+      }
+    },
+  });
+</script>
+
+<style lang="scss" module>
+  .image {
+  }
+</style>
