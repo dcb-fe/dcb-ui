a2d28dd5bc3f7307e541e3cca32e0d9048e4fd30
WangYanzhi0525 <75294703+WangYanzhi0525@users.noreply.github.com>
2021-09-14 15:01:13 +0800

    feat: 新增 steps 组件 (#48)

---
+++ src/components/image/image.md
@@ -0,0 +1,7 @@
+# Image
+
+## 基础渲染
+
+<demo src="image/basic" />
+
+<api src="image" />
---
+++ src/components/steps/__demo__/custom.vue
@@ -0,0 +1,24 @@
+<template>
+  <d-steps
+    @click="handleClick"
+    :activeIcon="'arrow_right'"
+    :activeColor = "'#FF960A'"
+    :inActiveIcon="'arrow_right'"
+  >
+    <d-step>买家下单</d-step>
+    <d-step>商家接单</d-step>
+    <d-step>买家提货</d-step>
+    <d-step>交易完成</d-step>
+    <d-step>用户评价</d-step>
+  </d-steps>
+</template>
+
+<script>
+  export default {
+    methods: {
+      handleClick() {
+        alert('hello');
+      },
+    },
+  };
+</script>
