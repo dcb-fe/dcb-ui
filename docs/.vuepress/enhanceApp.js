23ae295b7119e2427a5d9226c641ab511e3e215d
高荣君 <gaorongjun@gogpay.cn>
2022-01-10 14:50:23 +0800

    feat: 新增mobile 查看组件功能

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,7 +3,88 @@ import * as demos from './demo';
 import ApiTable from './apiTable';
 import ComponentContributors from './componentContributors';
 
-export default ({ Vue }) => {
+export default ({ Vue, router }) => {
+  window.demos = demos;
+  const createMobileDemo = to => {
+    const mobile = document.getElementById('mobile-demo');
+    if (mobile) {
+      mobile.remove();
+    }
+    const mobileDemoDiv = document.createElement('div');
+    mobileDemoDiv.id = 'mobile-demo';
+    mobileDemoDiv.style.position = 'fixed';
+    mobileDemoDiv.style.width = '375px';
+    mobileDemoDiv.style.height = '640px';
+    mobileDemoDiv.style.zIndex = '999';
+    mobileDemoDiv.style.right = '16px';
+    mobileDemoDiv.style.top = '3.6rem';
+    mobileDemoDiv.style.padding = '102px 28px 106px';
+    mobileDemoDiv.style.backgroundImage =
+      "url('https://assets.gogpay.cn/web/onlineview/img/model.png')";
+    mobileDemoDiv.style.backgroundSize = '100% 100%';
+    mobileDemoDiv.style.zIndex = '999';
+    const mobileDemoDivMask = document.createElement('div');
+    mobileDemoDivMask.style.width = '375px';
+    mobileDemoDivMask.style.height = '640px';
+    mobileDemoDivMask.style.background = '#fff';
+    mobileDemoDivMask.style.position = 'absolute';
+    mobileDemoDivMask.style.zIndex = '0';
+    const mobileDemo = document.createElement('iframe');
+    mobileDemo.style.position = 'relative';
+    mobileDemo.id = 'mobile-iframe';
+    mobileDemo.style.width = '375px';
+    mobileDemo.style.height = '100%';
+    mobileDemo.style.border = '0';
+    mobileDemo.style.zIndex = '1';
+    mobileDemo.src = `${
+      window.location.origin
+    }/dcb-ui/v0/mobile/mobile.html?routerName=${
+      to.fullPath.split('/components/')[1]
+    }`;
+    mobileDemoDiv.appendChild(mobileDemoDivMask);
+    mobileDemoDiv.appendChild(mobileDemo);
+    return mobileDemoDiv;
+  };
+  router.beforeEach((to, from, next) => {
+    if (to.name && to.path.indexOf('/components/') !== -1) {
+      if (!document.getElementById('mobile-demo')) {
+        let timd = '';
+        const add = () => {
+          if (
+            document.getElementsByClassName('theme-container')[0] &&
+            location.pathname.split('/components/')[1]
+          ) {
+            document
+              .getElementsByClassName('theme-container')[0]
+              .appendChild(createMobileDemo(to));
+            clearTimeout(timd);
+          } else {
+            add();
+          }
+        };
+        timd = setTimeout(() => add(), 500);
+      } else {
+        if (
+          document.getElementsByClassName('theme-container')[0] &&
+          location.pathname.split('/components/')[1]
+        ) {
+          document
+            .getElementsByClassName('theme-container')[0]
+            .appendChild(createMobileDemo(to));
+        }
+      }
+    } else if (
+      to.path.indexOf('/components/') === -1 &&
+      to.path.indexOf('/mobile/') === -1
+    ) {
+      const mobile = document.getElementById('mobile-demo');
+      if (mobile) {
+        mobile.remove();
+      }
+    }
+    next();
+  });
+
   Vue.use(DcbUI);
   Object.keys(demos).forEach(name => {
     Vue.component(name, demos[name]);
---
+++ docs/mobile/mobile.md
@@ -0,0 +1,95 @@
+<template>
+  <div id="mobile"
+       style="width: 100%;
+       height: 100%;
+       position: fixed;
+       left: 0;
+       top: 0;
+       margin: 0;
+       z-index: 999999;
+       border-radius: 12px;
+       background: #fff;
+       overflow-y: scroll;
+       box-sizing: border-box;
+       ">
+    <demo
+      v-if="isIframe"
+    ></demo>
+    <ul class="menu-list" v-else>
+      <li v-for="item in list" @click="showDemo(item)"><span>{{item.title}}</span><d-icon name="arrow_right" /></li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+  import router from './router.js'
+  let list = []
+  Object.keys(router).map(key => {
+    if (key.indexOf('.html#') !== -1) {
+      list.push({
+        title: key.replace('.html#', '-'),
+        key,
+        routerName: router[key]
+      })
+    } 
+  })
+  let getQueryString = (name) => {
+    let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+      return unescape(r[2]);
+    }
+    return null;
+  }
+  let href = parent.document.getElementById("mobile-demo") ? parent.document.getElementById("mobile-iframe").contentWindow.location.href : window.location.href
+  let nowRoute = href.split('routerName=')[1]
+  let routerName = router[decodeURI(nowRoute)] || getQueryString('routerName')
+  let menuKey = getQueryString('menuKey')
+  let mobileIframe = parent.document.getElementById("mobile-demo")
+  export default {
+    name: 'test',
+    components: {
+      'demo': demos[routerName]
+    },
+    data() {
+      return {
+        isIframe: mobileIframe || menuKey ? 1: 0,
+        list,
+        src: '333'
+      }
+    },
+    mounted() {
+      let meta = document.createElement('meta')
+      meta.name = 'viewport'
+      meta.content = 'width=device-width, maximum-scale=1, user-scalable=no'
+      document.head.appendChild(meta)
+      document.getElementById('mobile').parentNode.parentNode.style.padding = 0
+    },
+    methods: {
+      showDemo (info) {
+        window.location.href = window.location.origin + '/dcb-ui/v0/mobile/mobile.html?&menuKey='+ info.title +'&routerName=' + info.key
+      }
+    }
+  };
+</script>
+<style>
+  #mobile::-webkit-scrollbar {
+    display: none
+  }
+  .menu-list{
+    padding: 0;
+  }
+  .menu-list > li{
+    display: flex;
+    align-items: center;
+    justify-content:space-between;
+    padding: 8px;
+    border-bottom: 1px solid #ccc;
+    color: #323233;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 40px;
+    background: #f7f8fa;
+  }
+</style>
